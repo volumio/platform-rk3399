fa0f26d111181f9ae9089d6978e9cd086d5647aa
Gé Koerkamp <ge.koerkamp@gmail.com>
2023-11-10 22:00:10 +0100

    Refactored with Armian integration using mkplatform.sh

--- README.md
+++ README.md
@@ -1,101 +1,20 @@
-## Prerequisites for building with Armbian
 
-- x64 machine with at least 2GB of memory and ~35GB of disk space for a VM, container or native OS,
-- Ubuntu Hirsute 21.04 x64 for native building or any [Docker](https://docs.armbian.com/Developer-Guide_Building-with-Docker/) capable x64 Linux for containerised,
-  - Hirsute is required for newer non-LTS releases.. ex: Bullseye, Sid, Groovy, Hirsute
-  - If building for LTS releases.. ex: Focal, Bionic, Buster, it is possible to use Ubuntu 20.04 Focal, but it is not supported
-- superuser rights (configured sudo or root access).
+# platform-meson64
+Volumio platform files for Amlogic S905Y2/ S905X3/ S922X and A311D (or other meson64) SBCs
 
-## How to build the Armbian kernel and u-boot for Volumio?
-
-Download the armbian build system
-```
-sudo apt-get install git
-git clone https://github.com/armbian/build armbian-volumio
-```
-Prepare the customized build script
-```
-cd armbian-volumio
-cat <<-EOF > compile-custom-nanopim4.sh
-sudo ./compile.sh  BOARD=nanopim4 BRANCH=current KERNEL_ONLY=yes KERNEL_CONFIGURE=yes KERNEL_KEEP_CONFIG=yes CREATE_PATCHES=yes
-EOF
-sudo chmod +x compile-custom-nanopim4.sh
-cd ..
-git clone https://github.com/gkkpch/platform-rk3399
-cd platform-rk3399
-tar xfJ nanopim4.tar.xz
-cd ..
-```
-
-## Start u-boot and kernel configurations
-```
-cd armbian-volumio
-./compile-custom-nanopim4.sh
-```
-This will download all further prerequisites.  
-Once finished downloading and all patches have been applied, you get the opportunity to add your own patches (valid both for u-boot and kernel).  
-
-The first patch break is before compiling u-boot, now modify the sources in *armbian-volumio/output/cache/sources/u-boot/*.  
-After you have done the modifications (or don't have any) press \<Enter>.  
-
-The next patch break will be before compiling the kernel.  
-**Important:**  
-With the very first compile, you need to 
-- copy ```linux-rockchip64-current.config``` from *platform-rk3399/nanopim4/armbian* to *armbian-volumio/output/config*.
-- copy ```rk3399-nanopi-m4b.dts``` from *platform-rk3399/nanopim4/armbian* to *armbian-volumio/output/cache/linux-mainline/linux-5.x.y/arch/arm64/boot/dts/rockchip*
-- modify ```Makefile``` in *armbian/output/cache/sources/linux-mainline/linux-5.x.y/arch/arm64/boot/dts/rockchip* to compile the new dts.  
-
-**Alternative method for first compile**
-- copy ```linux-rockchip64-current.config``` from *platform-rk3399/nanopim4/armbian* to *armbian-volumio/output/config*.
-- copy ```kernel-rockchip64-current.patch``` from *platform-rk3399/nanopim4/armbian* to *armbian-volumio/output/patch*.
-
-After you have done any other modifications in *output/cache/sources/linux-mainline/linux-5.x.y* (or don't have any) press \<Enter>.  
-==> You will find your patches here: *armbian-volumio/output/patch/kernel-rockchip64-current.patch*, it is incremental.
-
-Next step is Kernel Configuration, just <exit> when you do not want to modify anything.  
-**Note 1:** Modified kernel settings will be saved for the next build.  
-**Note 2:** When you want to keep a backup of kernel configurations and patches, copy them from *armbian/output/patch/* and *armbian/output/config*  
-
-## Recompiling
-
-Restart the u-boot and kernel compilation.  
-```
-./compile-custom-nanopim4.sh
-```
-This time you only pay attention to the 2 patch breaks and the kernel config.
-Ignore them when there are no changes.
+Platform files are created using the armbian build system for kernel, u-boot, armbian-firmware.  
+They have specific Volumio settings in armbianEnv.txt (read by the generic Armbian boot script for meson64 devices).
 
 
-
-## When finished
-
-Move the following .deb packages from *armbian/output/debs* to *platform-rk3399/nanopim4/*
-```
-armbian-firmware_x.y.z-trunk_all.deb
-linux-headers-current-rockchip64_x.y.z-trunk_arm64.deb
-linux-image-current-rockchip64_x.y.z-trunk_arm64.deb
-linux-u-boot-current-nanopim4_x.y.z-trunk_arm64.deb
-```
-Where x.y.z is the used armbian version.  
-(At the time of writing 21.11.0)  
-Refer to ```armbian-firmware-full_x.y.z-trunk_all.deb``` for a full copy of all current firmware for 5.x.y  
-
-## Generating platform files
-
-This will be part of the nanopim4 build recipe.  
-It will unpack firmware, image and u-boot debs and put the information straight into the right place.
-
-## Changelog
-
-<sub>  
-  
 |Date|Author|Change
 |---|---|---|
-|16.12.2021|gkkpch|Initial
-|12.01.2022|gkkpch|Finished kernel buildscript
-|21.01.2022|gkkpch|Switched to rk3399-nanopi-m4b.dtb
-|||Kernel 5.10.93, switched kernel configuration
-|||Added kernel bluetooth support
-|||Added ```bcrm_patchram_plus``` and ```rk3399-bluetooth.service``` 
-|||Abandoned
-|29.02.2023|gkkpch|Revived with kernel 5.15,y, fixed alsa & bluetooth issues
+20230514|gkkpch|initial with Radxa Zero
+20230607||Added Radxa Zero2
+20230614||Modified radxa zero and zero2 dts to enable SPDIF
+|||Created corresponding audio routing settings with a modified,  board-specific asound.state
+|20230805|gkkpch|Added support for Odroid C4 and N2/N2+
+|||Refactored platform-radxa to platform-meson64
+|||Fixed mkplatform.sh text string, to reflect meson64, not just radxa
+|20230810|gkkpch|Enabled HDMI/Lineout/SPDIF output options
+|||Added support for Ugreen BT adapter, USB_DEVICE(0x0b05, 0x17dc)
+|||Fixed an issue with missing firmware for RTL8761BUV
---
+++ mkplatform.sh
@@ -0,0 +1,177 @@
+#!/bin/bash
+set -eo pipefail
+
+[[ $# -ge 1 ]] && shift 1
+if [[ $# -ge 0 ]]; then
+  armbian_extra_flags=("$@")
+  echo "Passing additional args to Armbian ${armbian_extra_flags[*]}"
+else
+  armbian_extra_flags=("")
+fi
+
+C=$(pwd)
+A=../armbian-rockchip64
+B="current"
+K="rockchip64"
+T="nanopim4"
+KERNELPATCH="no"
+KERNELCONFIGURE="no"
+
+# Make sure we grab the right version
+ARMBIAN_VERSION=$(cat ${A}/VERSION)
+export INSTALL_MOD_STRIP=1
+
+# Custom patches
+echo "Adding custom patches"
+
+mkdir -p "${A}"/userpatches/kernel/"${K}"-"${B}"/
+rm -rf "${A}"/userpatches/kernel/"${K}"-"${B}"/*.patch
+FILES="${C}"/patches/*.patch
+shopt -s nullglob
+for file in $FILES; do
+  cp $file "${A}"/userpatches/kernel/"${K}"-"${B}"/
+done
+
+# Custom kernel Config
+if [ -e "${C}"/kernel-config/linux-"${K}"-"${B}".config ]
+then
+  echo "Copy custom Kernel config"
+  rm -rf  "${A}"/userpatches/linux-"${K}"-"${B}".config
+  cp "${C}"/kernel-config/linux-"${K}"-"${B}".config "${A}"/userpatches/
+fi
+
+# Select specific Kernel and/or U-Boot version
+rm -rf "${A}"/userpatches/lib.config
+if [ -e "${C}"/kernel-ver/"${K}".config ]
+then
+  echo "Copy specific kernel/uboot version config"
+  cp "${C}"/kernel-ver/"${K}"*.config "${A}"/userpatches/lib.config
+fi
+
+if [ -d "${A}"/output/debs ]; then
+  echo "Cleaning previous .deb builds"
+  rm "${A}"/output/debs/*
+fi
+
+cd ${A}
+ARMBIAN_HASH=$(git rev-parse --short HEAD)
+echo "Building for $T -- with Armbian ${ARMBIAN_VERSION} -- $B"
+
+./compile.sh ARTIFACT_IGNORE_CACHE=yes BOARD=${T} BRANCH=${B} uboot 
+
+if [ $KERNELPATCH == yes ]; then
+  ./compile.sh ARTIFACT_IGNORE_CACHE=yes BOARD=${T} BRANCH=${B} kernel-patch 
+fi
+if [ $KERNELCONFIGURE == yes ]; then
+  ./compile.sh ARTIFACT_IGNORE_CACHE=yes BOARD=${T} BRANCH=${B} kernel-config 
+  cp "${A}"/userpatches/linux-"${K}"-"${B}".config "${C}"/kernel-config/
+fi
+
+./compile.sh CLEAN_LEVEL=images,debs,make-kernel ARTIFACT_IGNORE_CACHE=yes BOARD=${T} BRANCH=${B} kernel
+
+./compile.sh ARTIFACT_IGNORE_CACHE=yes BOARD=${T} BRANCH=${B} firmware 
+
+echo "Done!"
+
+cd "${C}"
+echo "Creating platform ${T} files"
+[[ -d ${T} ]] && rm -rf "${T}"
+mkdir -p "${T}"/boot/overlay-user
+mkdir -p "${T}"/lib/firmware
+mkdir -p "${T}"/lib/systemd/system/
+mkdir -p "${T}"/u-boot
+mkdir -p "${T}"/var/lib/alsa
+
+# Copy asound.state
+if [ -f "${C}/audio-routing/${T}-asound.state" ]; then
+  cp "${C}/audio-routing/${T}-asound.state" "${T}"/var/lib/alsa/asound.state
+fi
+
+# Keep a copy for later just in case
+#cp "${A}/output/debs/linux-headers-${B}-${K}_${ARMBIAN_VERSION}"* "${C}"
+
+echo "${A}/output/debs/linux-dtb-${B}-${K}_${ARMBIAN_VERSION}"*.deb
+dpkg-deb -x "${A}/output/debs/linux-dtb-${B}-${K}_${ARMBIAN_VERSION}"*.deb "${T}"
+echo "${A}/output/debs/linux-image-${B}-${K}_${ARMBIAN_VERSION}"*.deb
+dpkg-deb -x "${A}/output/debs/linux-image-${B}-${K}_${ARMBIAN_VERSION}"*.deb "${T}"
+echo "${A}/output/debs/linux-u-boot-${T}-${B}_${ARMBIAN_VERSION}"*.deb
+dpkg-deb -x "${A}/output/debs/linux-u-boot-${T}-${B}_${ARMBIAN_VERSION}"*.deb "${T}"
+echo "${A}/output/debs/armbian-firmware_${ARMBIAN_VERSION}"*.deb
+dpkg-deb -x "${A}/output/debs/armbian-firmware_${ARMBIAN_VERSION}"*.deb "${T}"
+echo "Remove unused firmware"
+rm -r "${T}"/lib/firmware/qcom
+rm "${T}"/lib/firmware/dvb*
+
+
+cp "${T}"/usr/lib/linux-u-boot-${B}-${T}/* "${T}/u-boot/"
+cp "${T}"/usr/lib/u-boot/platform_install.sh "${T}/u-boot/"
+
+mv "${T}"/boot/dtb* "${T}"/boot/dtb
+mv "${T}"/boot/vmlinuz* "${T}"/boot/Image
+
+# Copy any additional firmware
+cp -r "${C}"/firmware "${T}"/lib
+
+echo "Add 'brcmfmac43456-sdio.friendlyarm,nanopim4' smlinks" 
+ln -s "${T}"/lib/firmware/brcm/brcmfmac43456-sdio.bin "${T}"/lib/firmware/brcm/brcmfmac43456-sdio.friendlyarm,nanopi4.bin 
+ln -s "${T}"/lib/firmware/brcm/brcmfmac43456-sdio.txt "${T}"/lib/firmware/brcm/brcmfmac43456-sdio.friendlyarm,nanopi4.txt 
+
+# Add additional services
+# Bluetooth for most of others (custom patchram is needed only in legacy)
+cp "${A}"/packages/bsp/rk3399/brcm_patchram_plus_rk3399 "${T}"/usr/bin
+cp "${A}"/packages/bsp/rk3399/rk3399-bluetooth.service "${T}"/lib/systemd/system/
+
+# Clean up unneeded parts
+rm -rf "${T}/lib/firmware/.git"
+rm -rf "${T:?}/usr" "${T:?}/etc"
+
+# Compile and copy over overlay(s) files
+for dts in "${C}"/overlay-user/overlays-"${T}"/*.dts; do
+  dts_file=${dts%%.*}
+  if [ -s "${dts_file}.dts" ]
+  then
+    echo "Compiling ${dts_file}"
+    dtc -O dtb -o "${dts_file}.dtbo" "${dts_file}.dts"
+    cp "${dts_file}.dtbo" "${T}"/boot/overlay-user
+  fi
+done
+
+# Copy and compile boot script
+if [ -f "${C}"/bootparams/boot-"${T}".cmd ]; then
+  cp "${C}"/bootparams/boot-"${T}".cmd "${T}"/boot/boot.cmd
+else
+  cp "${A}"/config/bootscripts/boot-"${K}".cmd "${T}"/boot/boot.cmd
+fi
+mkimage -C none -A arm -T script -d "${T}"/boot/boot.cmd "${T}"/boot/boot.scr
+
+# Signal mainline kernel
+touch "${T}"/boot/.next
+
+# Prepare boot parameters
+cp "${C}"/bootparams/armbianEnv-"${T}".txt "${T}"/boot/armbianEnv.txt
+
+
+# Signal mainline kernel
+touch "${T}"/boot/.next
+
+# Prepare boot parameters
+ls 
+cp "${C}"/bootparams/armbianEnv-"${T}".txt "${T}"/boot/armbianEnv.txt
+
+echo "Creating device tarball.."
+tar cJf "${T}_${B}.tar.xz" "$T"
+
+echo "Renaming tarball for Build scripts to pick things up"
+mv "${T}_${B}.tar.xz" "${T}.tar.xz"
+KERNEL_VERSION="$(basename ./"${T}"/boot/config-*)"
+KERNEL_VERSION=${KERNEL_VERSION#*-}
+echo "Creating a version file Kernel: ${KERNEL_VERSION}"
+cat <<EOF >"${C}/version"
+BUILD_DATE=$(date +"%m-%d-%Y")
+ARMBIAN_VERSION=${ARMBIAN_VERSION}
+ARMBIAN_HASH=${ARMBIAN_HASH}
+KERNEL_VERSION=${KERNEL_VERSION}
+EOF
+
+echo "Cleaning up.."
+rm -rf "${T}"
